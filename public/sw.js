932bb3c4a1a5041a41b64970d3c20857ff78ce3e
agalvez-tech <agalvez@inmobiliariapalanca.com>
2026-08-14 13:36:38 +0200

    Delete public directory

--- public/sw.js
+++
@@ -1,5 +0,0 @@
-// Service worker mínimo, solo para permitir "Añadir a pantalla de inicio".
-// No cachea nada de forma agresiva para evitar versiones desactualizadas.
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', () => {});
